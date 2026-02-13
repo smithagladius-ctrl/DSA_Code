b1f5479617ff041a3defc6d38de2e45417c6291b
smithagladius-ctrl <smithagladius@gmail.com>
2026-02-13 06:39:14 +0530

    Daily DSA: Bubble Sort (13/02/2026)

---
+++ solutions/13/02/2026_Bubble_Sort.java
@@ -0,0 +1,1 @@
+public class BubbleSort { public static void sort(int[] arr) { for(int i = 0; i < arr.length; i++) for(int j = 0; j < arr.length - 1 - i; j++) if(arr[j] > arr[j+1]) { int temp = arr[j]; arr[j] = arr[j+1]; arr[j+1] = temp; } } }
