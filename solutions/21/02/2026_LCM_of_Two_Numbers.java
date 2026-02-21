60fe0bb988d1161e8f1fae8c9272001b6814d0c1
smithagladius-ctrl <smithagladius@gmail.com>
2026-02-21 08:15:20 +0530

    Daily DSA: LCM of Two Numbers (21/02/2026)

---
+++ solutions/21/02/2026_LCM_of_Two_Numbers.java
@@ -0,0 +1,1 @@
+public class LCM { public static int lcm(int a, int b) { return (a * b) / gcd(a, b); } public static int gcd(int a, int b) { while(b != 0) { int temp = b; b = a % b; a = temp; } return a; } }
