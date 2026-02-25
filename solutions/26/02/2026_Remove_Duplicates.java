03cba471764c13871d1aa606e541e96e2ef11bc0
smithagladius-ctrl <smithagladius@gmail.com>
2026-02-26 00:58:33 +0530

    Daily DSA: Remove Duplicates (26/02/2026)

---
+++ solutions/26/02/2026_Remove_Duplicates.java
@@ -0,0 +1,1 @@
+public class RemoveDuplicates { public static int removeDuplicates(int[] arr) { int n = arr.length; int j = 0; for(int i = 1; i < n; i++) if(arr[i] != arr[j]) arr[++j] = arr[i]; return j + 1; } }
