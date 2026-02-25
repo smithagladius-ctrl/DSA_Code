c1a13d702b10f3364b1caff161d4f7b33593d298
smithagladius-ctrl <smithagladius@gmail.com>
2026-02-25 09:11:37 +0530

    Daily DSA: Duplicate Elements (25/02/2026)

---
+++ solutions/25/02/2026_Duplicate_Elements.java
@@ -0,0 +1,1 @@
+public class DuplicateElements { public static void findDuplicates(int[] arr) { for(int i = 0; i < arr.length; i++) for(int j = i + 1; j < arr.length; j++) if(arr[i] == arr[j]) System.out.println(arr[i]); } }
