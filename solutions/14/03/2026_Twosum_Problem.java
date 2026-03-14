fdd4e0ac0e82f64ac81a01fab531fe3f0d5542b7
smithagladius-ctrl <smithagladius@gmail.com>
2026-03-14 23:32:54 +0530

    Daily DSA: Twosum Problem (14/03/2026)

---
+++ solutions/14/03/2026_Twosum_Problem.java
@@ -0,0 +1,1 @@
+public class TwoSum { public static int[] twoSum(int[] nums, int target) { java.util.Map<Integer,Integer> map = new java.util.HashMap<>(); for(int i = 0; i < nums.length; i++) { int complement = target - nums[i]; if(map.containsKey(complement)) return new int[]{map.get(complement), i}; map.put(nums[i], i); } return new int[]{-1,-1}; } }
