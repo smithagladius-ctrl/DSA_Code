148b74dcde0f1a80d5cb8a740fe3ddb7ae67f88d
smithagladius-ctrl <smithagladius@gmail.com>
2026-03-18 19:15:37 +0530

    Daily DSA: Product of Array Except Self (18/03/2026)

---
+++ solutions/18/03/2026_Product_of_Array_Except_Self.java
@@ -0,0 +1,1 @@
+public class ProductArray { public static int[] productExceptSelf(int[] nums) { int[] result = new int[nums.length]; int prefix = 1, suffix = 1; for(int i = 0; i < nums.length; i++) result[i] = 1; for(int i = 0; i < nums.length; i++) { result[i] *= prefix; prefix *= nums[i]; } for(int i = nums.length - 1; i >= 0; i--) { result[i] *= suffix; suffix *= nums[i]; } return result; } }
