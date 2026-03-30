8779d49242b168fea5f16455ab7dd6e2a791f9ca
smithagladius-ctrl <smithagladius@gmail.com>
2026-03-30 22:31:05 +0530

    Daily DSA: Floyd Warshall (30/03/2026)

---
+++ solutions/30/03/2026_Floyd_Warshall.java
@@ -0,0 +1,1 @@
+public class FloydWarshall { public static void floydWarshall(int[][] graph) { int n = graph.length; for(int k = 0; k < n; k++) for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]); } }
