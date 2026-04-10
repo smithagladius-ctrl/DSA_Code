4f27f201ccf055bafba77fc9de60933d51cd6579
smithagladius-ctrl <smithagladius@gmail.com>
2026-04-10 05:41:32 +0530

    Daily DSA: Backtracking N Queens (10/04/2026)

---
+++ solutions/10/04/2026_Backtracking_N_Queens.java
@@ -0,0 +1,1 @@
+public class NQueens { public static boolean isSafe(int[][] board, int row, int col, int n) { for(int i = 0; i < col; i++) if(board[row][i] == 1) return false; for(int i = row, j = col; i >= 0 && j >= 0; i--, j--) if(board[i][j] == 1) return false; for(int i = row, j = col; i < n && j >= 0; i++, j--) if(board[i][j] == 1) return false; return true; } }
